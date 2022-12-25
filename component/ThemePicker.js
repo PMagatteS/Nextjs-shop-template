04d272586db9e76cac36850f5cf2dfb34326882e
PMagatteS <papamagattesady@gmail.com>
2022-12-25 12:55:19 +0000

    Done with the theme picker logic i will add more options

--- component/ThemePicker.js
+++ component/ThemePicker.js
@@ -1,19 +1,31 @@
 import React from 'react';
-import {MdBrush} from 'react-icons/md'
+import {MdBrush, MdClose} from 'react-icons/md'
+import { useStateContext } from '../context/useStateContext'
 
-const ThemeBoard =  () => {
+const ThemeBoard =  ({themes, togglePicker, setThemeColors}) => {
     return(
-        <div>
+<div className="theme-board">
+    <MdClose className='close-picker' onClick={togglePicker}/>
 
-        </div>
+    <h5>Theme color</h5>
+    <div className="theme-colors">        
+        {themes.colors.map((el, i) => <div className="colors" key={i} style={{width:30, height:30, backgroundColor:el, border:"1px solid black"}} onClick={(e) => setThemeColors(el, 'theme')} ></div>)}
+    </div>
+    <h5>Font color</h5>
+    <div className="font-colors">
+        {themes.fonts.map((el, i) => <div className="colors" key={i} style={{width:30, height:30, backgroundColor:el, border:"1px solid black"}} onClick={(e) => setThemeColors(el, 'font')}></div>)}        
+    </div>
+</div>
     )
 }
 
 const ThemePicker = () => {
+    const {showPicker, togglePicker, themes, setThemeColors} = useStateContext()
     return (
-        
-            <MdBrush className="theme-icon"/>
-        
+     <>
+        {showPicker&&<ThemeBoard themes={themes} togglePicker={togglePicker} setThemeColors={setThemeColors}/>}
+        <MdBrush className="theme-icon" onClick={togglePicker}/>        
+     </>   
     );
 };
 
